e621c131c308916d1339036ef8ff168af0bfc73d
Simon Højberg <r.hackr@gmail.com>
2021-06-05 00:01:22 -0400

    fix GitHub spelling

--- src/App.elm
+++ src/App.elm
@@ -382,11 +382,11 @@ viewPublishModal =
                 (section
                     []
                     [ p [ class "main" ]
-                        [ text "With your Unison codebase on Github, open a Pull Request against "
+                        [ text "With your Unison codebase on GitHub, open a Pull Request against "
                         , a [ href "https://github.com/unisonweb/shipwright/edit/trunk/files/initialize-codebase.sh", rel "noopener", target "_blank" ] [ text "this file" ]
                         , text " to list (or unlist) your project on Unison Share."
                         ]
-                    , a [ class "help", href "https://www.unisonweb.org/docs/codebase-organization/#day-to-day-development-creating-and-merging-pull-requests", rel "noopener", target "_blank" ] [ text "How do I get my code on Github?" ]
+                    , a [ class "help", href "https://www.unisonweb.org/docs/codebase-organization/#day-to-day-development-creating-and-merging-pull-requests", rel "noopener", target "_blank" ] [ text "How do I get my code on GitHub?" ]
                     ]
                 )
     in
