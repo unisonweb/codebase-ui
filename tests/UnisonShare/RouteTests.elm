cb4ce4e5a04b07db92fadaeb74651a75af8cad5e
Simon Højberg <r.hackr@gmail.com>
2022-01-07 10:57:04 -0500

    Enable the Catalog as the front page of Share

--- tests/UnisonShare/RouteTests.elm
+++ tests/UnisonShare/RouteTests.elm
@@ -21,16 +21,13 @@ catalogRoute =
                         mkUrl "/catalog"
                 in
                 Expect.equal Catalog (Route.fromUrl "" url)
-
-        {- TODO enable when cutting over to catalog
-           , test "Matches root to Catalog" <|
-               \_ ->
-                   let
-                       url =
-                           mkUrl "/"
-                   in
-                   Expect.equal Catalog (Route.fromUrl "" url)
-        -}
+        , test "Matches root to Catalog" <|
+            \_ ->
+                let
+                    url =
+                        mkUrl "/"
+                in
+                Expect.equal Catalog (Route.fromUrl "" url)
         ]
 
 
